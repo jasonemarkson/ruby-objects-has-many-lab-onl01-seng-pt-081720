827efaadb43b1db035fa86991fa29edd909241b8
Jason Markson <jasonmarkson76@gmail.com>
2020-09-12 19:33:52 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,8 +16,8 @@ class Artist
     song.artist = self
   end
   
-  def add_song_by_name(song, artist)
-    song = Song.new(song, artist)
+  def add_song_by_name(song)
+    song = Song.new(song)
     add_song(song)
   end
 
